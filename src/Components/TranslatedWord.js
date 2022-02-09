5a4d136e094ce0ce451a74695165899ea489d1c0
brew doctor <>
2022-02-09 17:48:17 -0500

    reversed arabic characters from JSON

--- src/Components/TranslatedWord.js
+++ src/Components/TranslatedWord.js
@@ -1,26 +1,34 @@
-import JSONDATA from "/Users/mariamaosoble/ada/Ebarat_Project_Frontend/ebarat_project_frontend/src/Data.json";
+import JSONDATA from "/Users/mariamaosoble/ada/Ebarat_Project_Frontend/ebarat_project_frontend/src/related.json";
 import React, { useState, useEffect } from "react";
 
 function TranslatedWord({ searchTerm }) {
   let translation = "";
   let related_words = "";
-  for (const some_Object of JSONDATA) {
-    const word = some_Object["word"];
-    if (word === searchTerm) {
-      console.log(some_Object["translation"]);
-      translation = some_Object["translation"];
-      related_words = some_Object["similarWords"];
-      console.log(some_Object["similarWords"]);
-    }
-  }
 
+  const word = JSONDATA["word"];
+  if (word === searchTerm) {
+    translation = JSONDATA["translated_word"];
+    related_words = JSONDATA["similarWords"];
+    console.log(related_words);
+  }
+  function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+  }
+  let reversed_word_list = [];
+  for (const related_word of related_words) {
+    console.log(related_word);
+    reversed_word_list.push(reverseString(related_word));
+  }
   return (
     "Translation of " +
     searchTerm +
     " is " +
     translation +
     " and some related words are " +
-    related_words
+    reversed_word_list
   );
 }
 export default TranslatedWord;
