48eca1139eb615cf1819fb5f4fac9b6a2ba44d04
brew doctor <>
2022-02-09 23:39:16 -0500

    made a word cloud and created an ordered list from the word array

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
           </div>
           <div className="related-words-area">
             <TranslatedRelatedWords searchTerm={searchTerm} />
+            
             {/* for word in related words: put a button */}
 
             {/* <div class="lds-ellipsis">
